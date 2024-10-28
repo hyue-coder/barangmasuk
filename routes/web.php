e5faee3f9174f7d70692ad62b0bb5396ebe9f3bc
wahyunawawi <113686834+wahyunawawi@users.noreply.github.com>
2024-10-28 13:10:53 +0700

    file baru

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+Route::get('anjay', function () {
+    return view('welcome');
+});
+
+Route::post('/login', [LoginController::class, 'login']);
