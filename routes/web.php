cc41c4982484a1c8f9873c9200b6ae18f53e8f90
wahyunawawi <113686834+wahyunawawi@users.noreply.github.com>
2024-11-02 13:48:01 +0700

    new file

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,24 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('login');
 });
-Route::get('anjay', function () {
-    return view('welcome');
-});
 
+//ini route login
 Route::post('/login', [LoginController::class, 'login']);
-Auth::routes();
+// ini route admin
+Route::get('/admin', [AdminController::class, 'admin']);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// ini route barang
+Route::get('/admin/barang', [AdminController::class, 'barang']); //ini untuk menampilkan data
+Route::get('/admin/barang/tambah', [AdminController::class, 'tambah_barang']); //ini untuk menampilkan form tambah
+Route::get('/admin/barang/edit/{id}', [AdminController::class, 'edit_barang']); //ini untuk menampilkan form edit
+Route::get('/admin/barang/delete/{id}', [AdminController::class, 'delete_barang']); //ini untuk menghapus data
+Route::post('/admin/barang/simpan', [AdminController::class, 'simpan_barang']); //ini untuk menyimpan data
+Route::post('/admin/barang/update/{id}', [AdminController::class, 'update_barang']); //ini untuk mengupdate data
+
+
